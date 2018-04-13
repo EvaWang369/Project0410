85b50f6cfd9f7c516c7cd0341b8c3c01fac7979f
EvaWang369 <mevaw369@gmail.com>
2018-04-13 14:25:57 -0700

    update the threads number

--- suppression/src/suppression/Project.java
+++ suppression/src/suppression/Project.java
@@ -23,7 +23,7 @@ public class Project implements Runnable{
 	
 	static {
 		try {
-			br = new BufferedReader (new FileReader("/Users/mengwang/Desktop/split/ss/xab.txt"), 10);
+			br = new BufferedReader (new FileReader("/Users/mengwang/Desktop/Suppression/Android/xac.txt"));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -36,45 +36,38 @@ public class Project implements Runnable{
 		String temp = null;
 		StringBuffer sb = new StringBuffer();
 		while (true) {
-			this.list = new ArrayList<String>();
-			synchronized(br) {
+			//synchronized(br) {
 				try {
 					while((line = br.readLine()) != null) {
-						
+						synchronized(line) {
 							System.out.println(line + " " + count + Thread.currentThread().getName());
 							//apple_idfa, google_ad_id
-							url = new URL ("http://service.sponsorpay.com/installs/v2?appid=cc7dbb7fee183785fe3e25afdbb42c62&answer_received=0&install_attributed=0&apple_idfa=" + line + "&SE_script");
+							url = new URL ("http://service.sponsorpay.com/installs/v2?appid=40d67e58e66c50905f0c1c21948c184c&answer_received=0&install_attributed=0&google_ad_id=" + line + "&SE_script");
 							
 							HttpURLConnection con = (HttpURLConnection) url.openConnection();
 							con.setRequestMethod("GET");
 							con.setRequestProperty("User-Agent", USER_AGENT);
-							System.out.println (url);
+							//System.out.println (url);
 							count++;
-/*
 							int responseCode = con.getResponseCode();
-					       // System.out.println("\nSending 'GET' request to URL : " + url);
-					       // System.out.println("Response Code : " + responseCode + "count: " + count);
-				            //getTitle (sb, temp, url);*/
+							System.out.println(responseCode);
+							
 							try {
-								Thread.sleep(1/10000);
+								Thread.sleep(1/500);
 							} catch (InterruptedException e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
+						}
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+			//}
 			}
-			/*
-			try {
-				Thread.sleep(1/100);
-				System.out.println(this.list);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}*/
-			if(line == null)
-				break;
+			//if(line == null)
+			//	break;
 		}
-	}
+
+
 }
--- suppression/src/suppression/sup.java
+++ suppression/src/suppression/sup.java
@@ -4,18 +4,30 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class sup {
+public class sup extends Thread{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//MyRunnable mr = new MyRunnable();
-		Project mr = new Project();
-		
+		//Project mr = new Project();
+		/*
 		Thread t1 = new Thread (new Project(), "A");
 		Thread t2 = new Thread (new Project(), "B");
-		
-		t1.start();
+		Thread t3 = new Thread (new Project(), "C");
+		Thread t4 = new Thread (new Project(), "D");
+		Thread t5 = new Thread (new Project(), "E");
+		Thread t6 = new Thread (new Project(), "F");
+		Thread t7 = new Thread (new Project(), "G");
 		t2.start();
+		t1.start();
+		t3.start();
+		t4.start();
+		t5.start();
+		t6.start();
+		t7.start();*/
+		for (int i = 0; i < 100; i++) {
+	        new Thread(new Project()).start();
+	    }
 		
 		//File file = new File ("/Users/mengwang/Desktop/Copy2.txt");
 		
